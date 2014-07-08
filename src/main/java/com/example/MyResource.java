c467cea8655139f2642c45a5a01bc76438d0f80b
Gustavo Bazan <gssbzn@gmail.com>
2014-07-08 07:06:20 -0430

    Cambio uri

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.core.MediaType;
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("/")
 public class MyResource {
 
     /**
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Hello, Heroku!";
+        return "Hello World!";
     }
 }
