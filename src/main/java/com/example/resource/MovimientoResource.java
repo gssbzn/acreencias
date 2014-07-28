24337aa8f218ce22d50919535398cea756ea5ae5
Gustavo Bazan <gssbzn@gmail.com>
2014-07-28 15:07:51 -0430

    Mejoras al manejo y creacion de movimientos

--- src/main/java/com/example/resource/MovimientoResource.java
+++ src/main/java/com/example/resource/MovimientoResource.java
@@ -13,22 +13,28 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.example.dao.ClienteDAO;
 import com.example.dao.CuentaDAO;
 import com.example.dao.DAOFactory;
 import com.example.dao.MovimientoDAO;
+import com.example.model.Cliente;
 import com.example.model.Cuenta;
 import com.example.model.Movimiento;
 
 @Path("clientes/{cliente_id}/cuentas/{cuentas_id}/movimientos")
 public class MovimientoResource {
-	private final MovimientoDAO dao;
-	private final CuentaDAO cuentaDAO;
+	private final ClienteDAO clienteDao;
+	private final CuentaDAO cuentaDao;
+	private final MovimientoDAO movimientoDao;
+	
+	
 	
 	private static final Logger logger = Logger.getLogger(MovimientoResource.class.toString());
 
     public MovimientoResource(){		
-        dao = DAOFactory.getMovimientoDAO();
-        cuentaDAO = DAOFactory.getCuentaDAO();
+        movimientoDao = DAOFactory.getMovimientoDAO();
+        clienteDao = DAOFactory.getClienteDAO();
+        cuentaDao = DAOFactory.getCuentaDAO();
     }
     
     @GET
@@ -36,7 +42,7 @@ public class MovimientoResource {
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Movimiento show(@PathParam("cuenta_id") Integer cuenta_id, @PathParam("id") Integer id) {	
 		logger.info("SHOW");
-		Movimiento movimiento = dao.find(id);
+		Movimiento movimiento = movimientoDao.find(id);
 		if(movimiento == null)
 			throw new WebApplicationException(404);
 		return movimiento;
@@ -46,20 +52,24 @@ public class MovimientoResource {
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public List<Movimiento> index(@PathParam("cliente_id") Integer cliente_id, @PathParam("cuenta_id") Integer cuenta_id) {
     	logger.info("SHOW-ALL");    	
-        return dao.findMovimientosCuenta(cuenta_id);
+        return movimientoDao.findMovimientosCuenta(cuenta_id);
     }
     
     @POST    
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response create(@PathParam("cuenta_id") Integer cuenta_id, Movimiento entity) {
+    public Response create(@PathParam("cliente_id") Integer cliente_id, @PathParam("cuenta_id") Integer cuenta_id, Movimiento entity) {
     	logger.info("CREATE");
-    	Cuenta cuenta = cuentaDAO.find(cuenta_id);
-    	entity.setCuenta(cuenta);
-    	Movimiento mov = (Movimiento) dao.create(entity);   	
-    	System.out.println("Nuevo Movimiento creado: Monto: "+mov.getMonto()+" Tipo: "+mov.getTipo()+" Cuenta: "+mov.getCuenta().getId());
-    	cuentaDAO.actualizarCuenta(mov);
-    	System.out.println("Saldo Actualizado: "+mov.getCuenta().getSaldo()+" Cuenta id: "+mov.getCuenta().getId());
+    	Cliente cliente = clienteDao.find(cliente_id);		
+        Cuenta cuenta = cuentaDao.find(cuenta_id);
+        if(cliente == null || cuenta == null)
+            throw new WebApplicationException(404);
+    	
+        entity.setCuenta(cuenta);
+    	Movimiento mov = (Movimiento) movimientoDao.create(entity);   	
+    	logger.info("Nuevo Movimiento creado: Monto: "+mov.getMonto()+" Tipo: "+mov.getTipo()+" Cuenta: "+mov.getCuenta().getId());
+    	cuentaDao.actualizarCuenta(mov);
+    	logger.info("Saldo Actualizado: "+mov.getCuenta().getSaldo()+" Cuenta id: "+mov.getCuenta().getId());
         return Response.created(null).entity(mov).build();
     }
     
